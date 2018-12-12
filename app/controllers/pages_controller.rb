c5c1b4a24d3c7e1fa40a1547c4a9dc202cffe295
hezi2 <codydotyh@gmail.com>
2018-12-11 23:37:21 -0500

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -56,6 +56,6 @@ class PagesController < ApplicationController
 				@councils_empty = Council.includes(:memberships).where(memberships: { id: nil })
 			end
 		end
-		@councils = @councils_ordered
+		@councils = @councils_ordered + @councils_empty
 	end
 end
